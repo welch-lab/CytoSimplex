33e60dee5262d2304193ccddbcdcef6e16a5a9ee
Yichen Wang <wayichen@umich.edu>
2025-04-22 12:45:45 -0400

    More fixes towards R-4.5.0 requirement

--- R/data.R
+++ R/data.R
@@ -1,5 +1,5 @@
 #' Mouse bone marrow scRNAseq example data
-#' @format \linkS4class{dgCMatrix} object
+#' @format \link[Matrix]{dgCMatrix-class} object
 #' @source https://www.nature.com/articles/s41467-023-38034-2
 #' @references Matsushita, Y., Liu, J., Chu, A.K.Y. et al. Bone marrow endosteal
 #' stem cells dictate active osteogenesis and aggressive tumorigenesis. Nat
@@ -15,7 +15,7 @@
 "rnaCluster"
 
 #' Velocity graph of the example mouse bone marrow data
-#' @format \linkS4class{dgCMatrix} object
+#' @format \link[Matrix]{dgCMatrix-class} object
 #' @source https://www.nature.com/articles/s41467-023-38034-2
 #' @references Matsushita, Y., Liu, J., Chu, A.K.Y. et al. Bone marrow endosteal
 #' stem cells dictate active osteogenesis and aggressive tumorigenesis. Nat
